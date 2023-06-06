6e8dcb22d9ef86931b9db9be009cbcd672b2f5de
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-06-07 01:31:12 +0630

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+// console.log("Hello World");
 
 app.get('/', (req, res) => {
- res.send("Hello Express")
+ res.sendFile(__dirname + "/views/index.html");
 })
 
 
